ffe1f79039d6544390aa7bf05951c70691d50a63
Timur Galeev <timur_galeev@outlook.com>
2021-08-04 21:42:54 +0200

    Added Jenkins

---
+++ jenkins/jenkins-master/groovy-scripts/autostatus-plugin.groovy
@@ -0,0 +1,8 @@
+import jenkins.model.Jenkins
+import org.jenkinsci.plugins.githubautostatus.BuildStatusConfig
+
+// We want github-autostatus plugin 'Send to Github' disabled by default
+def buildStatus = BuildStatusConfig.get()
+buildStatus.getEnableGithub()
+buildStatus.setEnableGithub(false)
+
